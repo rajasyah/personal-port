043222442949d1a31d5c45549387ece36a0a5061
rajasyah <rajasyahacp20@gmail.com>
2022-03-29 15:23:51 +0700

    add image and feature download cv

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Header = () => {
+  const [open, setOpen] = useState(false);
+  const nav = [
+    {
+      name: "Home",
+      to: "/",
+    },
+    {
+      name: "About",
+      to: "/about",
+    },
+    {
+      name: "Education",
+      to: "/education",
+    },
+    {
+      name: "Experience",
+      to: "/experience",
+    },
+    {
+      name: "Work",
+      to: "/work",
+    },
+    {
+      name: "Contact",
+      to: "/contact",
+    },
+  ];
+
   return (
     <div className="bg-gray-900 h-16 w-full flex items-center justify-between">
       <div className="left pl-28">
@@ -11,24 +39,11 @@ const Header = () => {
       </div>
       <div className="right pr-28">
         <ul className="flex font-body text-white text-lg font-semibold gap-10">
-          <li className="text-yellow-300">
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/education">Education</Link>
-          </li>
-          <li>
-            <Link to="/experience">Experience</Link>
-          </li>
-          <li>
-            <Link to="/work">Work</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
+          {nav.map((item) => (
+            <li key={item.name}>
+              <Link to={item.to}>{item.name}</Link>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import img from "../assets/rajasyah.png";
+import doc from "../assets/rajasyah-portfolio.pdf";
 
 const Home = () => {
   return (
     <div className="container-page">
-      <h1 className="text-3xl text-white">Home</h1>
+      <div>
+        <p className="text-white font-body">Hello</p>
+        <h1 className="text-4xl text-white font-body font-bold mb-5">
+          I'm <span className="text-red-500">Rajasyah</span> Pahlevi
+        </h1>
+        <Link
+          to={doc}
+          download
+          target="_blank"
+          className="text-white font-bold border-white border-2 p-2 rounded-lg hover:bg-red-800"
+        >
+          Download CV
+        </Link>
+      </div>
+      <div>
+        <img src={img} alt="rajasyah" />
+      </div>
+      <div>
+        <h1 className="text-4xl text-red-500 font-bold">Frontend Developer</h1>
+      </div>
     </div>
   );
 };
