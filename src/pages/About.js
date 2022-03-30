41e10577eedc10a835a133a7f98567b2355f0ccf
rajasyah <rajasyahacp20@gmail.com>
2022-03-30 20:04:09 +0700

    route update about pages

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,10 +5,10 @@ const About = () => {
   return (
     <div className="container-page flex-col">
       <div className="flex gap-10">
-        <h1 className="text-xl text-white hover:bg-red-900 border-2 px-2 py-1 rounded-md">
-          <Link to="/about">About Me</Link>
+        <h1 className="text-xl text-white hover:bg-gray-800 border-b-2 px-2 py-1 rounded-md">
+          <Link to="about">About Me</Link>
         </h1>
-        <h1 className="text-xl text-white hover:bg-red-900 border-2 px-2 py-1 rounded-md">
+        <h1 className="text-xl text-white hover:bg-gray-800 border-b-2 px-2 py-1 rounded-md">
           <Link to="skills">My Skills</Link>
         </h1>
       </div>
