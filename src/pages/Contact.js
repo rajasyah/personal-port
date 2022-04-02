1e297f799a522fc8d45d1ff0de9d478668bb8bd7
rajasyah <rajasyahacp20@gmail.com>
2022-04-02 15:09:42 +0700

    adding library email js and sweetalert2

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,49 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import Swal from "sweetalert2";
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_d1wyt17",
+        "template_licqs1b",
+        form.current,
+        "_z8QXPPZa4pGtwsGt"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          Swal.fire({
+            title: "Message Sent Successfully",
+            icon: "success",
+          });
+        },
+        (error) => {
+          Swal.fire({
+            title: "Ooops, something went wrong",
+            text: error.text,
+            icon: "error",
+          });
+        }
+      );
+    e.target.reset();
+  };
   return (
     <div className="container-page">
-      <h1 className="text-3xl text-white">Contact</h1>
+      <form ref={form} onSubmit={sendEmail} className="bg-blue-500">
+        <label>Name</label>
+        <input type="text" name="user_name" />
+        <label>Email</label>
+        <input type="email" name="user_email" />
+        <label>Message</label>
+        <textarea name="message" />
+        <input type="submit" value="Send" />
+      </form>
     </div>
   );
 };
