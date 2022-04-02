2aa41de198026f62bde2a9a530d98a39dcca65e0
rajasyah <rajasyahacp20@gmail.com>
2022-04-02 18:50:43 +0700

    complete contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -21,6 +21,8 @@ const Contact = () => {
           Swal.fire({
             title: "Message Sent Successfully",
             icon: "success",
+            showConfirmButton: false,
+            timer: 2000,
           });
         },
         (error) => {
@@ -35,14 +37,37 @@ const Contact = () => {
   };
   return (
     <div className="container-page">
-      <form ref={form} onSubmit={sendEmail} className="bg-blue-500">
+      <form
+        ref={form}
+        onSubmit={sendEmail}
+        className="flex flex-col gap-5 w-2/6 h-5/6 p-10 bg-red-700 text-white font-body rounded-lg shadow-black shadow-lg"
+      >
+        <h1 className="text-xl text-center font-bold">Contact us</h1>
         <label>Name</label>
-        <input type="text" name="user_name" />
+        <input
+          type="text"
+          name="user_name"
+          placeholder="Type name here ..."
+          className="text-gray-800 px-2 py-1 rounded-md focus:outline-none"
+        />
         <label>Email</label>
-        <input type="email" name="user_email" />
+        <input
+          type="email"
+          name="user_email"
+          placeholder="Type email here ..."
+          className="text-gray-800 px-2 py-1 rounded-md focus:outline-none"
+        />
         <label>Message</label>
-        <textarea name="message" />
-        <input type="submit" value="Send" />
+        <textarea
+          name="message"
+          placeholder="Type messages here ..."
+          className="h-full rounded-lg py-2 px-4 text-gray-800 focus:outline-none"
+        />
+        <input
+          type="submit"
+          value="Send"
+          className="bg-gray-900 rounded-lg py-1"
+        />
       </form>
     </div>
   );
